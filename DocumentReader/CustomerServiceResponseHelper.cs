a58d2c9328173737a5fe1f98d27bd0e8e1e01518
Adam Tovatt <sporedenstore@hotmail.com>
2024-08-01 16:28:01 +0200

    can respond with chatgpt

---
+++ DocumentReader/CustomerServiceResponseHelper.cs
@@ -0,0 +1,35 @@
+﻿using DocumentReader.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentReader
+{
+    public class CustomerServiceResponseHelper
+    {
+        private const string prompt = @"Du är en assistent som svarar på kunders frågor med hjälp och endast med hjälp av din interna dokumentation
+                                        som du får tillsammans med kundens fråga. Om frågan är oklar eller om det inte verkar som dokumentationen kan svara på den
+                                        ska du inte svara. Kom ihåg att alltid svara så kort som möjligt och trevligt.";
+
+        private OpenAiApi openAi;
+
+        public CustomerServiceResponseHelper()
+        {
+            openAi = new OpenAiApi(EnvironmentHelper.GetOpenAiApiKey());
+        }
+
+        public async Task<string> GetResponseToQuestionAsync(string question, string relevantContext)
+        {
+            CompletionParameter completionParameter = new CompletionParameter("gpt-3.5-turbo");
+
+            completionParameter.AddSystemMessage(prompt);
+            completionParameter.AddUserMessage($"Här är dokumentation som kan vara relevant:\n\r***\n\r{relevantContext}\n\r***\n\rHär är kundens fråga:\n\r{question}");
+
+            CompletionResult completionResult = await openAi.CompleteAsync(completionParameter);
+
+            return completionResult.Choices.First().Message.Content;
+        }
+    }
+}
--- DocumentReader/Program.cs
+++ DocumentReader/Program.cs
@@ -5,27 +5,31 @@ namespace DocumentReader
 {
     internal class Program
     {
+        private const string documentsDirectory = "documents";
+
         static async Task Main(string[] args)
         {
-            if(!Directory.Exists("documents"))
-                Directory.CreateDirectory("documents");
+            if(!Directory.Exists(documentsDirectory))
+                Directory.CreateDirectory(documentsDirectory);
 
             FileSystemStoredEmbeddingRepository embeddedDocumentsRepository = new FileSystemStoredEmbeddingRepository("documents_embedded");
             FileSystemStoredEmbeddingRepository userInputRepository = new FileSystemStoredEmbeddingRepository("user_input_embedded");
 
             VectorCollection<StoredEmbedding> embeddedDocuments = new VectorCollection<StoredEmbedding>();
 
-            foreach(string file in Directory.EnumerateFiles("documents"))
+            foreach(string file in Directory.EnumerateFiles(documentsDirectory))
             {
                 string fileContent = await FileHelper.GetFileContentAsync(file);
                 StoredEmbedding storedEmbedding = await EmbeddingHelper.Instance.GetEmbeddingAsync(fileContent, embeddedDocumentsRepository);
                 embeddedDocuments.Add(storedEmbedding);
             }
 
+            CustomerServiceResponseHelper customerServiceAi = new CustomerServiceResponseHelper();
+
             while (true)
             {
                 Console.Clear();
-                Console.WriteLine("Enter some text to compare to the documents:");
+                Console.WriteLine("Ställ din fråga till Retendo: ");
                 string? userInput = Console.ReadLine();
 
                 if(string.IsNullOrWhiteSpace(userInput))
@@ -35,8 +39,23 @@ namespace DocumentReader
 
                 StoredEmbedding queryResult = embeddedDocuments.FindNearest(userInputEmbedding.Coordinates, 1).First();
 
+                Console.WriteLine("Sökning i intern dokumentation klar.");
+                Console.WriteLine("Skriver ett svar...");
+
+                string aiResponse = await customerServiceAi.GetResponseToQuestionAsync(userInput, queryResult.TextContent);
+
+                Console.Clear();
+
+                Console.WriteLine("Din fråga:");
+                Console.WriteLine(userInput);
                 Console.WriteLine();
+
+                Console.WriteLine("Svar:");
+                Console.WriteLine(aiResponse);
+                Console.WriteLine();
+                Console.WriteLine("Baserat på dokumentet:");
                 Console.WriteLine(queryResult.TextContent);
+
                 Console.ReadLine();
             }
         }
